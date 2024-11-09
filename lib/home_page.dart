5ebf13077d31dc55bdbbef6f5a1c3adcdfa39d53
CLS11 <crystall.101600@gmail.com>
2024-11-09 11:53:31 +0530

    GridView.builder for displaying the buttons on the screen.

---
+++ lib/home_page.dart
@@ -0,0 +1,91 @@
+import 'package:calculator/buttons.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemCount: buttons.length,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return Buttons(
+                      color: Colors.green!,
+                      textColor: Colors.white,
+                      buttonText: buttons[index],
+                    );
+                  } else if (index == 1) {
+                    return Buttons(
+                      color: Colors.red!,
+                      textColor: Colors.white,
+                      buttonText: buttons[index],
+                    );
+                  } else {
+                    return Buttons(
+                      color: isOperator(buttons[index])
+                          ? Colors.deepPurple!
+                          : Colors.deepPurple[50]!,
+                      textColor: isOperator(buttons[index])
+                          ? Colors.white
+                          : Colors.deepPurple,
+                      buttonText: buttons[index],
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
